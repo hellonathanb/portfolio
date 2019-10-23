e789380131d3d404584be3127f403ba88777f575
Miranda <miranda.pegg@gmail.com>
2019-10-23 13:31:39 -0500

    file gathering

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,8 @@
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
+
+var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".service"), "85%");
+var stickyHeader = new StickyHeader();
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor:  "./app/assets/scripts/Vendor.js"
+  },
+
+  output: {
+
+    path: path.resolve(__dirname, "./app/temp/scripts"),
+
+    filename: "[name].js"
+
+    },
+	module: {
+	  rules: [
+		{
+		  test: /\.js$/,
+		  exclude: /(node_modules|bower_components)/,
+		  use: {
+			loader: 'babel-loader',
+			options: {
+			  presets: ['@babel/preset-env']
+			}
+		  }
+		}
+	  ]
+	}
+
+}
